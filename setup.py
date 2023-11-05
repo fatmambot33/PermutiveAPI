c17f569e9cc3367d13ea53b47f7da31c28c86f9e
fatmambo33 <fatmambo33@gmail.com>
2023-11-05 16:40:29 +0100

    modified:   setup.py 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
+import os
 from setuptools import setup, find_packages
 
+requirements = []
+requirements_file = 'requirements.txt'
+
+# Check if the file exists
+if os.path.exists(requirements_file):
+    with open(requirements_file) as f:
+        requirements = f.read().splitlines()
 setup(
     name='PermutiveAPI',
     version='v3.2.2',
     packages=find_packages(),
-    install_requires=[
-
-    ],
+    install_requires=requirements,
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
 )
