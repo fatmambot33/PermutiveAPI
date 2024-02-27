bcf7f34c798a04830b04488059b13a6599434a48
fatmambo33 <fatmambo33@gmail.com>
2024-02-27 10:11:10 +0100

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if os.path.exists(requirements_file):
         requirements = f.read().splitlines()
 setup(
     name='PermutiveAPI',
-    version='v3.5.0',
+    version='v3.5.1',
     packages=find_packages(),
     install_requires=requirements,
     long_description=open('README.md').read(),
